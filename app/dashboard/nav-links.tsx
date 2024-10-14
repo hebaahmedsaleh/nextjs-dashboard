3491ca642d3d1d8c5cf6f684f50f003d288c399c
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2024-10-14 22:51:36 +0300

    fix paths links

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -3,14 +3,25 @@
 import {
   UserGroupIcon,
   HomeIcon,
-  InboxIcon,
+  DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import clsx from "clsx";
 
 export default function NavLinks() {
   const pathname = usePathname();
+
+  // Map of links to display in the side navigation.
+  // Depending on the size of the application, this would be stored in a database.
+  const links = [
+    { name: "Home", href: "/dashboard", icon: HomeIcon },
+    {
+      name: "Invoices",
+      href: "/dashboard/invoices",
+      icon: DocumentDuplicateIcon,
+    },
+    { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+  ];
 
   return (
     <>
@@ -20,7 +31,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            classNamle={clsx(
+            className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
                 "bg-sky-100 text-blue-600": pathname === link.href,
